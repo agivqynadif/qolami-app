c8d9db1dda9dfc6e7edbbf56e5ea0646fdae2324
HeruPrayogo <heruprayogo062@gmail.com>
2024-03-25 21:26:25 +0700

    test

---
+++ app/src/main/java/com/test/qolami/view/account/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.test.qolami.view.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.test.qolami.R
+import com.test.qolami.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textDaftar.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.lupaPassword.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_resetPasswordFragment)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/test/qolami/view/account/LupaPasswordFragment.kt
@@ -0,0 +1,33 @@
+package com.test.qolami.view.account
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.test.qolami.R
+import com.test.qolami.databinding.FragmentLupaPasswordBinding
+
+
+
+class LupaPasswordFragment : Fragment() {
+    private lateinit var binding: FragmentLupaPasswordBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding= FragmentLupaPasswordBinding.inflate(layoutInflater, container, false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageBack.setOnClickListener {
+            findNavController().navigate(R.id.action_resetPasswordFragment_to_loginFragment)
+        }
+    }
+
+}
